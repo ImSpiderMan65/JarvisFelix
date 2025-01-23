4cd566b07975c122c33e7f63f4f0c93dec68cd6c
Miguel Felix Cazarez <145072414+ImSpiderMan65@users.noreply.github.com>
2025-01-23 09:57:40 -0800

    worked on it got to 1:09 in 3rd vidseo

--- JarvisP3/Assets/Graphs/Graph.cs
+++ JarvisP3/Assets/Graphs/Graph.cs
@@ -51,7 +51,7 @@ public class Graph
         }
 
         List<Node> open = new List<Node>();
-        List<Node> close = new List<Node>();
+        List<Node> closed = new List<Node>();
         float tentative_g_score = 0;
         bool tentative_is_better;
 
@@ -66,10 +66,50 @@ public class Graph
             Node thisNode = open[i];
             if (thisNode.getId() == endId)
             {
-                //ReconstructPath(start,end);
+                ReconstructPath(start,end);
                 return true;
             }
+
+            open.RemoveAt(i);
+            closed.Add(thisNode);
+            Node neighbour;
+            foreach (Edge e in thisNode.edgeList)
+            {
+                neighbour = e.endNode;
+                if (closed.IndexOf(neighbour) > -1)
+                    continue;
+                tentative_g_score = thisNode.g + distance(thisNode, neighbour);
+                if (open.IndexOf(neighbour)==-1)
+                {
+                    open.Add(neighbour);
+                    tentative_is_better = true;
+                }
+                else if (tentative_g_score < neighbour.g)
+                {
+                    tentative_is_better = true;
+                }
+                else
+                {
+                    tentative_is_better = false;
+                }
+
+                if (tentative_is_better)
+                {
+                    neighbour.cameFrom = thisNode;
+                    neighbour.g = tentative_g_score;
+                    neighbour.h = distance(thisNode, end);
+                    neighbour.f = neighbour.g + neighbour.h;
+
+                }
+            }
         }
+        return false;
+    }
+
+    public void ReconstructPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
     }
 
     float distance(Node a, Node b)
