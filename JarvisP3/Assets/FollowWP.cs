b5affe4c1d4ba782a4a2a3aa5175c88c573d1582
Miguel Felix Cazarez <145072414+ImSpiderMan65@users.noreply.github.com>
2025-01-27 11:43:08 -0800

    donw with waypoints

--- JarvisP3/Assets/FollowWP.cs
+++ JarvisP3/Assets/FollowWP.cs
@@ -6,7 +6,7 @@ public class FollowWP : MonoBehaviour
 {
     Transform goal;
     float speed = 5.0f;
-    float accuracy = 1.0f;
+    float accuracy = 5.0f;
     float rotSpeed = 2.0f;
 
     public GameObject wpManager;
@@ -22,11 +22,27 @@ public class FollowWP : MonoBehaviour
         wps = wpManager.GetComponent<wPManager>().waypoints;
         g = wpManager.GetComponent<wPManager>().graph;
         currentNode = wps[0];
+        //Invoke("GoToRuin", 2);
     }
 
     public void GotoHelli()
     {
         g.AStar(currentNode, wps[0]);
+        currentWP = 0;
+    }
+    public void GotoRefinary()
+    {
+        g.AStar(currentNode, wps[9]);
+        currentWP = 0;
+    }
+    public void Gotohouses()
+    {
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+    }
+    public void GotoOil()
+    {
+        g.AStar(currentNode, wps[8]);
         currentWP = 0;
     }
     public void GoToRuin()
@@ -36,8 +52,25 @@ public class FollowWP : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count) 
+            return;
+
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 }
